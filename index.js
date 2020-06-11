bb2a4dab4e57b025bc89fc26edcca0d782b04442
joannajjliu <joanna.liu3774@gmail.com>
2020-06-11 00:00:05 -0400

    messenger quick start

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 'use strict';
 
 // Imports dependencies and set up http server
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const request = require('request');
 const
     express = require('express'),
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json()); //creates express http server
 
+// Declaring variables:
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+// Handler functions:
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+  
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    } else if (received_message.attachments) {
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+    } 
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response);    
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+    }); 
+}
+  
 // Set server port and log message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 8080, () => {
+    console.log('webhook is listening');
+});
 
 // -- Add the webhook endpoint: -- //
 app.post('/webhook', (req, res) => {
@@ -23,6 +83,18 @@ app.post('/webhook', (req, res) => {
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
             console.log("webhook event: ", webhook_event);
+              
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);        
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
 
         //  Returns a '200 OK' response to all requests,
@@ -37,7 +109,7 @@ app.post('/webhook', (req, res) => {
 //-- Add webhook verification --
 // Support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-    let VERIFY_TOKEN = "me_123"
+    let VERIFY_TOKEN = 'me_123';
 
     // Parse the query params:
     let mode = req.query['hub.mode'];
