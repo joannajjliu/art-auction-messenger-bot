b115301c659435e9b99dd026a4fd446f7cd42653
joannajjliu <joanna.liu3774@gmail.com>
2020-06-13 00:43:53 -0400

    added postback buttons template, image handling, and sender_firstName extraction

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const request = require('request');
+const https = require('https');
 const
     express = require('express'),
     bodyParser = require('body-parser'),
@@ -13,6 +14,7 @@ const
 // Declaring variables:
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+let sender_firstName;
 // Handler functions:
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
@@ -20,7 +22,6 @@ function handleMessage(sender_psid, received_message) {
 
     // Check if the message contains text
     if (received_message.text) {
-  
       // Create the payload for a basic text message
       response = {
         "text": `You sent the message: "${received_message.text}". Now send me an image!`
@@ -28,15 +29,51 @@ function handleMessage(sender_psid, received_message) {
     } else if (received_message.attachments) {
         // Gets the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
-    } 
-    
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is this the right picture?",
+                  "subtitle": "Tap a button to answer.",
+                  "image_url": attachment_url,
+                  "buttons": [
+                    {
+                      "type": "postback",
+                      "title": "Yes!",
+                      "payload": "yes",
+                    },
+                    {
+                      "type": "postback",
+                      "title": "No!",
+                      "payload": "no",
+                    }
+                  ],
+                }]
+              }
+            }
+          }
+    }
     // Sends the response message
     callSendAPI(sender_psid, response);    
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    //Get the payload for the postback:
+    let payload = received_postback.payload;
+
+    //Set the response based on the postback payload
+    if (payload === "yes") {
+        response = { "text": "Thanks!" }
+    } else if (payload === "no") {
+        response = { "text": "Oops, try sending another image." }
+    }
+    //Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
@@ -48,7 +85,6 @@ function callSendAPI(sender_psid, response) {
     },
     "message": response
   }
-
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
@@ -72,13 +108,10 @@ app.listen(process.env.PORT || 8080, () => {
 // -- Add the webhook endpoint: -- //
 app.post('/webhook', (req, res) => {
     let body = req.body;
-
     // Checks if this is an event from a page subscription
     if (body.object === 'page') {
-
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
-
             // Gets the message, entry.messaging is an array, but
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
@@ -87,6 +120,9 @@ app.post('/webhook', (req, res) => {
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+            // assign to sender_firstName variable:
+            retrieveUserInfo(sender_psid);
+            console.log("firstName:", sender_firstName)
 
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
@@ -116,10 +152,8 @@ app.get('/webhook', (req, res) => {
 
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-
         // Checks the mode and token sent is correct
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
             // Responds with the challenge token from the request
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
@@ -129,5 +163,15 @@ app.get('/webhook', (req, res) => {
             res.sendStatus(403);
         }
     }
-
 })
+
+//api query returns sender first name, last name, and profile pic
+function retrieveUserInfo(sender_psid) {
+    const url = `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${process.env.PAGE_ACCESS_TOKEN}`
+    https.get(url, (response) => { 
+        response.on("data", (data) => { 
+            const userData = JSON.parse(data);
+            sender_firstName=userData.first_name;
+        })
+    })
+}
